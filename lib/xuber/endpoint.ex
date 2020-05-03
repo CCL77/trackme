3f511d34a4ed7296ec03cdb911cae74e986140fa
Cueli Claudio <clacue@xmedia.loc>
2020-05-03 15:46:21 +0200

    add socket timeout$

--- lib/xuber/endpoint.ex
+++ lib/xuber/endpoint.ex
@@ -1,8 +1,9 @@
 defmodule XUber.Endpoint do
   use Phoenix.Endpoint, otp_app: :xuber
 
-  socket "/socket", XUber.UserSocket
-
+  socket "/socket", XUber.UserSocket,
+    websocket: [timeout: 45_000],
+    longpoll: false
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
