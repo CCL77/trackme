7aa0dce1a18f54200d0ac1f27130a308c14fcce8
Cueli Claudio <clacue@xmedia.loc>
2020-05-03 15:35:08 +0200

    add host

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :xuber, XUber.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "fast-escarpment-11616.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
   # http: [port: {:system, "PORT"}],
   # url: [host: "example.com", port: 80],
   # cache_static_manifest: "priv/static/manifest.json"
