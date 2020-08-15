28146a107517e76c11714bcba4df6b42419ee3c4
Cueli Claudio <clacue@xmedia.loc>
2020-08-15 17:13:43 +0200

    update readme

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -45,14 +45,7 @@ function initializeMapAndLocator() {
     const new_lat = newlatlng.lat;
     const new_lng = newlatlng.lng;
 
-    // var Details = {
-    //     username: username,
-    //     active: true,
-    //     new_lat: new_lat,
-    //     new_lng: new_lng,
-    //     update: true
-    // };
-
+  
     channel.push(("move"), {
       name: name,
       coords: {
@@ -95,166 +88,10 @@ function initializeMapAndLocator() {
     console.log(resp.name)
     console.log(`Moved ${resp.name} to ${resp.coords.latitude}, ${resp.coords.longitude}`, resp.coords);
   })
-    
-
-
-
-    // if (map.hasLayer(mymarker)) {
-    //     map.removeLayer(mymarker);
-    // }
-
-    // mymarker = new L.Marker(e.latlng, {
-    //     icon: yellowIcon,
-    //     draggable: true
-    // });
-
-    // map.addLayer(mymarker);
-    // mymarker.bindPopup('<p>You are here ' + username + '</p>').openPopup();
   
-
-
 }
   
 initializeMapAndLocator()
 
-
-
-
-
-
-// function positionChangedStart(position) {
-
-//   let coords = position.coords;
-//   if(navigator.geolocation){
-//     map.setView([coords.latitude,coords.longitude], 13);
-//     marker = L.circle([coords.latitude,coords.longitude], {radius:1000})
-//     marker.id = name
-//     marker.addTo(map);
-//   }
-//   else {
-//     alert("Sorry, browser does not support geolocation!");
-//   }
-  
-
-  // if (marker.id == name){
-  //   marker.setLatLng([coords.latitude,coords.longitude])
-  // }
-  // else{
-  //   map.setView([coords.latitude,coords.longitude], 13);
-  //   marker = L.circle([coords.latitude,coords.longitude], {radius:1000})
-  //   marker.id = name
-  //   marker.addTo(map);
-
-  // }
-  // map = L.map('map').setView([coords.latitude,coords.longitude], 13)
-  
-  // markers[name] = L.marker([coords.latitude,coords.longitude], {title: name}).addTo(map);
-  // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  // }).addTo(map);
-  // marker[name]._icon.id = name;
-  // marker.setLatLng([coords.latitude,coords.longitude])
-  // L.marker([coords.latitude,coords.longitude]).addTo(map);
-
-
-  // channel.push(("move"), {
-  //   name: name,
-  //   coords: {
-  //     latitude: coords.latitude,
-  //     longitude: coords.longitude
-  //   }
-  // })
-// }
-
-
-// function positionChanged(position) {
-//   let coords = position.coords;
-
-//   channel.push(("move"), {
-//     name: name,
-//     coords: {
-//       latitude: coords.latitude,
-//       longitude: coords.longitude
-//     }
-//   })
-//   // map = L.map('map').setView([coords.latitude,coords.longitude], 13)
-//   // marker[name]._icon.id = name;
-//   // marker.setLatLng([coords.latitude,coords.longitude])
-//   // L.marker([coords.latitude,coords.longitude]).addTo(map);
-// }
-
-
-// map = L.map('map')
-//   // map.setView([coords.latitude,coords.longitude], 13)
-
-//   // marker = L.circle([51.505, -0.09], {radius:1000})
-//   // marker.id = name
-//   // marker.addTo(map);
-
-//   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//   }).addTo(map);
-//   // L.marker([coordsStart.latitude,coordsStart.longitude]).addTo(map);
-// // window.initMap = function() {
-// //   map = new google.maps.Map(document.getElementById('map'), {
-// //     center: {lat: -34.397, lng: 150.644},
-// //     zoom: 8
-// //   });
-// // }
-
-// // markers[name] = L.marker([51.505, -0.09], {title: name}).addTo(map);
-
-// // window.initMap = function() {
-// //   map = L.map('map').setView([51.505, -0.09], 13)
-// // }
-
-// // map = L.map('map').setView([51.505, -0.09], 13)
-
-// // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-// //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// // }).addTo(map);
-// // marker = L.marker([51.5, -0.09]).addTo(map);
-
-// channel.join()
-//   .receive("ok", resp => {
-
-//     // GeolocationPosition.coords(positionChangedStart)
-//     navigator.geolocation.watchPosition(positionChangedStart);
-//     // navigator.geolocation.watchPosition(positionChangedStart);
-
-//     // console.log(`Moved ${resp.name} to ${resp.coords.latitude}, ${resp.coords.longitude}`, resp.coords);
-//     // map.setView([coords.latitude,coords.longitude], 13);
-//     // marker = L.circle([coords.latitude,coords.longitude], {radius:1000})
-//     // marker.id = name
-//     // marker.addTo(map);
-    
-    
-    
-//     console.log("Joined successfully", resp)
-//   })
-//   .receive("error", resp => { console.log("Unable to join", resp) })
-
-// channel.on("welcome", resp => {
-//   console.log(`Welcome! Folks we know about: ${resp.names.join(', ')}`)
-// })
-
-// channel.on("joined", resp => {
-//   console.log(`${resp.names.join(', ')} has joined`)
-// })
-
-// // channel.on("left", resp => {
-// //   // L.marker([resp.coords.latitude,resp.coords.longitude]).remove(map);
-// //   console.log(`${resp.names.join(', ')} has left`)
-// // })
-
-// channel.on("moved", resp => {
-//   navigator.geolocation.watchPosition(positionChanged);
-//   console.log(marker.id)
-//   // if (marker.id == resp.name){
-//   //   marker.setLatLng([resp.coords.latitude,resp.coords.longitude])
-//   // }
-//   console.log(resp.name)
-//   console.log(`Moved ${resp.name} to ${resp.coords.latitude}, ${resp.coords.longitude}`, resp.coords);
-// })
 
 export default socket
